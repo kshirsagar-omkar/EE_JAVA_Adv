fc0b119f1c5092d08166587afb4bfada80563084
Omkar Kshirsagar <omkarkshirsagar2005@gmail.com>
2025-01-09 21:39:50 +0530

    HelloWorld

---
+++ JDBC_Codes/IntelliJ_IDEA/01HelloWorld/src/com/tca/Main.java
@@ -0,0 +1,49 @@
+package com.tca;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class Main {
+
+    public final static String URL = "jdbc:postgresql://localhost:5432/ee_java_db?sslmode=disable";
+    public final static String USER = "root";
+    public final static String PASSWORD = "root@123";
+    public final static String DRIVER = "org.postgresql.Driver";
+
+
+    public static Connection conn = null;
+    public static PreparedStatement ps = null;
+
+    public static void main(String[] args) {
+
+        try{
+
+            Class.forName(DRIVER);
+
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+
+            ps = conn.prepareStatement("INSERT INTO student VALUES(106, 'FFF', 99.87)");
+            ps.executeUpdate();
+
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                conn.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+
+
+
+        System.out.println("Hello, World!");
+
+
+    }
+}
