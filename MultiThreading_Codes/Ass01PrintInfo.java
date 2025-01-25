55579f28bbb5a06957c7d2ff92ac9576f38b2363
Omkar Kshirsagar <omkarkshirsagar2005@gmail.com>
2025-01-26 01:48:30 +0530

    Printing Information

---
+++ MultiThreading_Codes/Ass01PrintInfo.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+
+public class Ass01PrintInfo
+{
+	public static void main(String []args)
+	{
+		Thread t = new Thread();
+
+		System.out.println("\nThread Information: " + t);
+	}
+}
