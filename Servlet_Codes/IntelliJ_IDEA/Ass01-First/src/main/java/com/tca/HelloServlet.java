dcd14b542495394feb837d21006e9b48b794020c
Omkar Kshirsagar <omkarkshirsagar2005@gmail.com>
2025-01-29 23:45:14 +0530

    Helloworld Servlet Code

--- Servlet_Codes/IntelliJ_IDEA/Ass01-First/src/main/java/com/tca/HelloServlet.java
+++ Servlet_Codes/IntelliJ_IDEA/Ass01-First/src/main/java/com/tca/HelloServlet.java
@@ -1,4 +1,22 @@
 package com.tca;
 
-public class H {
+import java.io.*;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet({"/first", "/omkar"})
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("Hello World");
+        out.close();
+    }
+
 }
