f250ee1a9776d7c77e6bac03d8bea16f963517b0
Omkar Kshirsagar <omkarkshirsagar2005@gmail.com>
2025-02-26 00:03:57 +0530

    First JSP code

---
+++ Servlet_Codes/IntelliJ_IDEA/JSP-01-First-App/src/main/java/com/tca/First.java
@@ -0,0 +1,28 @@
+package com.tca;
+
+import java.io.*;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet({"/First"})
+public class First extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
---
+++ Servlet_Codes/IntelliJ_IDEA/JSP-01-First-App/src/main/webapp/First.jsp
@@ -0,0 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: omkar
+  Date: 2/25/25
+  Time: 11:57 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <%
+        int i=10;
+        int j=20;
+        int ans = i+j;
+        out.println("Ans :" + ans + " !");
+    %>
+</body>
+</html>
